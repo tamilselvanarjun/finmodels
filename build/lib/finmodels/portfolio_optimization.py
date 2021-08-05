7fea6621a7e46729e435cbe613da449c0fe2bdc1
brandonwilliams24 <twkiwxvjunzl@hotmail.com>
2021-08-05 10:18:20 +0500

    Improve performance

--- build/lib/finmodels/portfolio_optimization.py
+++ build/lib/finmodels/portfolio_optimization.py
@@ -19,10 +19,10 @@ def optimize_portfolio(expected_returns, covariance_matrix):
     expected_return = expected_returns @ weights
     risk = cp.quad_form(weights, covariance_matrix)
 
-    
+    # Define the objective function (maximize return, minimize risk)
     objective = cp.Maximize(expected_return - 0.5 * risk)
 
-    # Define the constraints (weights sum to 1, individual weights are non-negative)
+    
     constraints = [cp.sum(weights) == 1, weights >= 0]
 
     # Formulate and solve the problem
