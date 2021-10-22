bfcc0c61b3e91afd051f0a02387dd17cb6d9039a
wilsonlogan42 <gkkykkjv@hotmail.com>
2021-10-22 10:18:20 +0500

    Optimizing the code

--- finmodels/portfolio_optimization.py
+++ finmodels/portfolio_optimization.py
@@ -14,12 +14,12 @@ def optimize_portfolio(expected_returns, covariance_matrix):
     """
     num_assets = len(expected_returns)
 
-    # Define the variables for optimization
+    
     weights = cp.Variable(num_assets)
     expected_return = expected_returns @ weights
     risk = cp.quad_form(weights, covariance_matrix)
 
-    # Define the objective function (maximize return, minimize risk)
+    
     objective = cp.Maximize(expected_return - 0.5 * risk)
 
     # Define the constraints (weights sum to 1, individual weights are non-negative)
