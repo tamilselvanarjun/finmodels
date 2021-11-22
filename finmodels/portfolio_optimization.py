b42b038f29b79286cd656b6b1dfab112f42542e1
wilsonlogan42 <gkkykkjv@hotmail.com>
2021-11-22 10:18:20 +0500

    add unit test

--- finmodels/portfolio_optimization.py
+++ finmodels/portfolio_optimization.py
@@ -14,12 +14,12 @@ def optimize_portfolio(expected_returns, covariance_matrix):
     """
     num_assets = len(expected_returns)
 
-    
+    # Define the variables for optimization
     weights = cp.Variable(num_assets)
     expected_return = expected_returns @ weights
     risk = cp.quad_form(weights, covariance_matrix)
 
-    
+   
     objective = cp.Maximize(expected_return - 0.5 * risk)
 
     # Define the constraints (weights sum to 1, individual weights are non-negative)
