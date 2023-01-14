726b3315a24d8f382b3b486500d2fcddce8e5b4d
Tamilselvan A <42732673+tamilselvanarjun@users.noreply.github.com>
2023-01-15 01:02:00 +0530

    fixed the code with new changes

--- finmodels/dcf.py
+++ finmodels/dcf.py
@@ -3,11 +3,11 @@ def calculate_dcf(cash_flows, discount_rate):
     Calculate the Discounted Cash Flow (DCF) valuation.
 
     Parameters:
-    - cash_flows (list or array): List or array of future cash flows.
-    - discount_rate (float): Discount rate used in the calculation.
+    - cash_flows (list or array): List or array of future cash flows representing the cash inflows or outflows.
+    - discount_rate (float): The discount rate used to calculate the present value of future cash flows.
 
     Returns:
-    - float: DCF value.
+    - float: The present value of the cash flows based on the DCF valuation method.
     """
     dcf_value = sum(cf / (1 + discount_rate) ** i for i, cf in enumerate(cash_flows))
     return dcf_value
