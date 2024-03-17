740644aecb29439e734296c355385ac8307561c6
Tamilselvan Arjunan <nishantamil@gmail.com>
2024-03-17 19:48:21 +0530

    dcf the first cash flow should be discounted by 0 periods

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     
 setup(
     name='finmodels',
-    version='2.0.1',
+    version='2.0.2',
     packages=find_packages(),
     install_requires=[
         'numpy',
